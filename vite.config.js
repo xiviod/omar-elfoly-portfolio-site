dfd5001ef202b427361b21ade05300bf684f9dd0
xiviod <o.elfoly29@gmail.com>
2025-11-04 11:48:49 +0200

    chore: remove nested omar-elfoly-portfolio project and set version to 1.0.0

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/omar-elfoly-portfolio/',
+  base: '/omar-elfoly-portfolio-site/',
   plugins: [react()],
   build: {
     sourcemap: true,
